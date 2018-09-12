d8a1bdd3b4d3886c257fa6bf6fd4cb0c9b251f2f
ptirumala <ptirumala@zapoj.com>
2018-09-12 15:21:40 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,36 +1,55 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {Users} = require('./models/users');
+var {User} = require('./models/users');
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res)=> {
+app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
-  todo.save().then((doc)=> {
+
+  todo.save().then((doc) => {
     res.send(doc);
-  }, (e)=> {
+  }, (e) => {
     res.status(400).send(e);
   });
 });
 
-app.get('/todos',(req,res)=> {
-  Todo.find().then((todos)=>{
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
     res.send({todos});
-  }, (e)=>{
+  }, (e) => {
     res.status(400).send(e);
-  })
+  });
 });
 
-app.listen(3000,()=> {
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.listen(3000, () => {
   console.log('Started on port 3000');
 });
 
-module.exports={app};
+module.exports = {app};
