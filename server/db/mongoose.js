3322f9331b2efb3d29cc633ab73cda1a92f9c6c2
ptirumala <ptirumala@zapoj.com>
2018-09-15 12:42:49 +0530

    Setup separate test & development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.promise = global.promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true });
 
 module.exports = {mongoose};
