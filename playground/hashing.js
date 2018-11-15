b328fa373c7fcda40140b417bd1140c257ac92db
ptirumala <ptirumala@zapoj.com>
2018-11-15 10:59:47 +0530

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,39 +1,53 @@
-
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
 
-var data = {
-	id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, 'abc123');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decode = jwt.verify(token, 'abc123');
-console.log('decoded token:', decode);
+var hashedPassword = '$2a$10$huAU4qTnQuGPifHEXfV9cOmPJ7p61oKaoXrY1WviiDAznE/rW8oLK';
 
-
-// this token-hashing can be replaced with simple JWT as above
-// const{SHA256} = require('crypto-js');
-// var message = 'I am user prabhakar';
-// var hash = SHA256(message).toString();
-// console.log(`Message: ${message} \n Hash : ${hash}`);
+bcrypt.compare('123!', hashedPassword, (err, res) => {
+  console.log(res);
+});
 
 // var data = {
-// 	id: 4
+//   id: 10
 // };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
+//
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+
+// var data = {
+//   id: 4
+// };
 // var token = {
-// 	data,
-// 	hash : SHA256(JSON.stringify(data)+'some secrete').toString()
-// };
-
+//   data,
+//   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// }
+//
+//
 // // token.data.id = 5;
-// // token.hash = SHA256(JSON.stringify(data)).toString();
-
-// var resulthash =  SHA256(JSON.stringify(data)+'some secrete').toString();
-// if (resulthash === token.hash){
-// 	console.log('data was not changed');
+// // token.hash = SHA256(JSON.stringify(token.data)).toString();
+//
+//
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+// if (resultHash === token.hash) {
+//   console.log('Data was not changed');
 // } else {
-// 	console.log('data was changed, do not trust');
+//   console.log('Data was changed. Do not trust!');
 // }
